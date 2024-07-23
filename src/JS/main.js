409abad19674dc7ce6097a364e216e0ee894f6e6
Nur Farah <nuurfaarax15@gmail.com>
2024-07-23 11:35:04 +0300

    feat: Add Quran CSS and HTML files
    This commit adds the CSS file for styling the Quran page and the HTML file for displaying the Quran content.

---
+++ src/JS/main.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let container = document.querySelector(".container");
+    let loader = document.querySelector(".loader");
+
+    let structure = (surahNumber, surahEnglish, surahArabic) => {
+
+        container.innerHTML += `
+        <div class="surah-info">
+            <div class="surah-names">
+                <a href="" class="surah-en">${surahEnglish}</a>
+                <a href="" class="surah-ar">${surahArabic}</a>
+            </div>
+            <span class="ayah-number">${surahNumber}</span>
+        </div>`;
+
+    }
+
+    const surahSearch = (e) => {
+        let surahInfo = document.querySelectorAll('.surah-info');
+        let index = e.target.value.toUpperCase();
+
+        surahInfo.forEach((surah) => {
+            let surahEn = surah.querySelector(".surah-en").innerText.toUpperCase();
+            let surahAr = surah.querySelector(".surah-ar").innerText.toUpperCase();
+
+            if(surahEn.indexOf(index) > -1 || surahAr.indexOf(index) > -1) {
+                surah.style.display = 'flex';
+            }
+            else {
+                surah.style.display = 'none';
+            }
+
+        })
+    }
+
+    const getSurrah = async() => {
+        loader.style.display = 'block';
+        let response = await fetch("http://api.alquran.cloud/v1/quran/quran-uthmani");
+        let surah = await response.json();
+        loader.style.display = 'none';
+        surah.data.surahs.forEach((sura) => {
+            structure(sura.number, sura.englishName, sura.name);
+        })
+
+    }
+
+
+getSurrah();
+
+document.addEventListener('input', surahSearch)
+    
+})
