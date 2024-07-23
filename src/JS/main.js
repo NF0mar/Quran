59ea4be56942a1c1b737001eb834414ecc2cbfa5
Nur Farah <nuurfaarax15@gmail.com>
2024-07-24 00:30:43 +0300

    Update surah links to use relative URLs

--- src/JS/main.js
+++ src/JS/main.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
         container.innerHTML += `
         <div class="surah-info">
             <div class="surah-names">
-                <a href="/src/pages/quran.html?id=${surahNumber}" class="surah-en">${surahEnglish}</a>
-                <a href="/src/pages/quran.html?id=${surahNumber}" class="surah-ar">${surahArabic}</a>
+                <a href="../pages/quran.html?id=${surahNumber}" class="surah-en">${surahEnglish}</a>
+                <a href="../pages/quran.html?id=${surahNumber}" class="surah-ar">${surahArabic}</a>
             </div>
             <span class="ayah-number">${surahNumber}</span>
         </div>`;
@@ -35,10 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const getSurrah = async() => {
-        // loader.style.display = 'block';
+        loader.style.display = 'block';
         let response = await fetch("https://api.alquran.cloud/v1/quran/quran-uthmani");
         let surah = await response.json();
-        // loader.style.display = 'none';
+        loader.style.display = 'none';
         surah.data.surahs.forEach((sura) => {
             structure(sura.number, sura.englishName, sura.name);
         })
